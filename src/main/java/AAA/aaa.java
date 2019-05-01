832549aa38592efb203eedcd8f3d5cdb62b75cb6
genghang <eclipse_email@geng.com>
2019-05-01 12:55:00 +0800

    edit by hot_fix

--- src/main/java/AAA/aaa.java
+++ src/main/java/AAA/aaa.java
@@ -3,5 +3,6 @@ package AAA;
 public class aaa {
 	public static void main(String[] args) {
 		System.out.println("dasn");
+		System.out.println("das");
 	}
 }
---
+++ src/main/java/AAA/happy.java
@@ -0,0 +1,10 @@
+package AAA;
+
+public class happy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("happy!");
+	}
+
+}
