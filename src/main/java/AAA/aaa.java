47731fb346b1d74c04461741d30c73f79e3955de
gengHang <776129400@qq.com>
2019-05-02 23:06:48 +0800

    edit by genghang

--- src/main/java/AAA/aaa.java
+++ src/main/java/AAA/aaa.java
@@ -1,5 +1,9 @@
 package AAA;
 
-public class aaa {
+import sun.applet.Main;
 
+public class aaa {
+        public static void main(String args[]) {
+               System.out.println("Hello!!!!!!!!!!!!!!!!");
+ }
 }
